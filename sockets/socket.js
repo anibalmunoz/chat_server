cbe7217437d210bf3a43f8af82ec79b4a3250fdf
anibalmunoz <munoz4hernandez@gmail.com>
2023-03-08 15:44:55 -0600

    Autenticar cliente según el token enviado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,12 @@
+const { comprobarJWT } = require("../helpers/jwt");
 const { io } = require("../index");
 
 //Mensajes de Sockets
 io.on("connection", (client) => {
   console.log("Cliente conectado");
+  const [valido, uid] = comprobarJWT(client.handshake.headers["x-token"]);
+  if (!valido) return client.disconnect();
+  console.log("Cliente autenticado");
 
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
